0fe2560f17789c90dd65c98572b82bc3c5c46466
Aziz Zainutdin <aloha@scriptor.me>
2018-02-18 13:18:10 +0300

    DSGN-01: Немного причесал и опробовал BULMU

--- src/Core/Pages/MainPage.tsx
+++ src/Core/Pages/MainPage.tsx
@@ -1,29 +1,41 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { IAppState } from './../Models/CoreModels';
-import { IGreetingsState } from './../Models/GreetingsModel';
+import { IAppState, IGreetingsState } from 'Core/Models';
 
 interface IMainPageProps {
     greetings: IGreetingsState;
 }
 
 class MainPageView extends React.Component<IMainPageProps> {
-
     private handleClick = (): void => {
         console.log('Click!');
-    }
+    };
 
     public render(): JSX.Element {
         const { greetings } = this.props;
         return (
-            <div className="mainPage--container">
-                <h1>{greetings.title}</h1>
-                <button
-                    className="button is-outlined is-info is-rounded"
-                    onClick={this.handleClick}
-                >
-                    {greetings.home ? greetings.home.label : 'Hm'}
-                </button>
+            <div className="hero is-dark is-fullheight">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">{greetings.title}</h1>
+                        <button
+                            className="button is-outlined is-info is-rounded subtitle"
+                            onClick={this.handleClick}
+                        >
+                            {greetings.home ? greetings.home.label : 'Hm…'}
+                        </button>
+                    </div>
+                </div>
+                <div className="hero-foot mainPage--footer">
+                    <div className="level">
+                        <div className="level-left"></div>
+                        <div className="level-right">
+                            <a href="https://bulma.io" className="level-item">
+                                <img src="https://bulma.io/images/made-with-bulma--white.png" alt="Made with Bulma" width="128" height="24" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -33,7 +45,6 @@ const mapStateToProps = (state: IAppState): IMainPageProps => ({
     greetings: state.greetings
 });
 
-export const MainPage = connect<{}, IMainPageProps, {}>(
-    mapStateToProps,
-    null
-)(MainPageView);
+export const MainPage = connect<{}, IMainPageProps, {}>(mapStateToProps, null)(
+    MainPageView
+);
